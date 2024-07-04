4f2abd6c19082dc780c96bdfde58ece6f4fe5ab6
4thdraw <4thdraw2@gmail.com>
2024-07-04 09:45:01 +0900

    자소서만들기

---
+++ src/Introduce.js
@@ -0,0 +1,11 @@
+function Bby(){
+    return(
+        // jsx html태그 아님 xml임
+        <>
+            <p>내 인생의 첫 리액트~!!!!!<br></br> 오예!!!</p>
+            <p>내 인생의 첫 리액트~!!!!! 오예!!!</p>          
+        </>        
+    )
+}
+
+export default Bby;
